cee2b9630dc1c5b9901921d72d312b2cf28dd722
LuongKimPhuong2611 <18521275@gm.uit.edu.vn>
2020-11-28 09:30:25 +0700

    update image & fire brick

--- Game/PlayScene.cpp
+++ Game/PlayScene.cpp
@@ -39,7 +39,7 @@ PlayScene::PlayScene() : Scene()
 	keyHandler = new PlayScenceKeyHandler(this);
 	typeSophia = 1;
 	LoadBaseObjects();
-	ChooseMap(STAGE_1);
+	ChooseMap(STAGE_1*3);
 }
 
 void PlayScene::LoadBaseObjects()
